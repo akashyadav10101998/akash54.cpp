8a1071c4fc00374fcf6e608063a71b0479976c1b
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-05 16:04:27 +0530

    Add files via upload

---
+++ loop1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int  n ;
+cout << "print all the numbers from 1 to n";
+cin >> n;
+int i = 1;
+while (i <=100)
+{
+cout << "value of number" <<i<< endl; 
+
+ i = i+1;
+}
+return 0;
+}  
